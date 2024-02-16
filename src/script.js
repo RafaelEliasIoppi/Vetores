b4187360b7c9d5e0d2715ab7e609570f2fcdef26
Rafael Elias Ioppi <141973836+RafaelEliasIoppi@users.noreply.github.com>
2024-02-16 16:15:16 +0000

    rafael

--- src/script.js
+++ src/script.js
@@ -1,17 +1,22 @@
 function lerVetor() {
-    // Obtém o elemento de entrada pelo ID
     var inputElement = document.getElementById("inputVector");
-
-    // Obtém o valor do campo de entrada
     var vetorString = inputElement.value;
-
-    // Divide a string em um vetor de strings separadas por vírgula
     var vetor = vetorString.split(',');
-
-    // Converte as strings do vetor em números
     vetor = vetor.map(function(item) {
-          return parseInt(item.trim(), 10);
+        return parseInt(item.trim(), 10);
     });
+
     var outputElement = document.getElementById("output");
-    outputElement.innerText = "Vetor: " + vetor.join(', ');
+    outputElement.innerHTML = "<div class='output-container'><h2>Vetor Original</h2><p>" + vetor.join(', ') + "</p></div>";
+
+    var pares = vetor.filter(function(num) {
+        return num % 2 === 0;
+    });
+    var impares = vetor.filter(function(num) {
+        return num % 2 !== 0;
+    });
+
+    outputElement.innerHTML += "<div class='output-container'><h2>Pares</h2><p>" + pares.join(', ') + "</p></div>";
+
+    outputElement.innerHTML += "<div class='output-container'><h2>Ímpares</h2><p>" + impares.join(', ') + "</p></div>";
 }
